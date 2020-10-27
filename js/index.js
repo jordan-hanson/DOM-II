bda13952972059774cbdd802ba8589d5572d97dd
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-10-27 13:57:27 -0600

    onmouseover and onmouseout implemented

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const funBus = document.querySelector("h2.go")
+console.log(funBus)
+funBus.onmouseover = overLetsGo;
+funBus.onmouseout = outLetsGo
+function overLetsGo() {
+    funBus.classList.add("goUpdated")
+    funBus.innerHTML = "Your adventure begins"
+    // funBus.innerHTML = 'The Fun Bus underlined?'
+}
+function outLetsGo() {
+    funBus.classList.remove("goUpdated")
+}
